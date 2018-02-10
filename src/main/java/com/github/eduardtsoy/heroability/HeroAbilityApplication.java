f956b5fec8d7d5923ffa1b819dc6a85c737eb495
EduardTsoy <eduard.tsoy@gmail.com>
2018-02-10 13:29:28 +0300

    Initial project generated (with Spring Boot, Postgres, JAX-RS, embedded Tomcat, Flyway)

---
+++ src/main/java/com/github/eduardtsoy/heroability/HeroAbilityApplication.java
@@ -0,0 +1,12 @@
+package com.github.eduardtsoy.heroability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroAbilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroAbilityApplication.class, args);
+	}
+}
